e616f669bfa9f0f168df406713322a3a3693dfdf
Barkley Santo <barkley.santo@gmail.com>
2020-12-29 16:48:58 -0600

    adding style dor divs

--- src/App.vue
+++ src/App.vue
@@ -34,5 +34,31 @@ export default {
 
   #app{
     background-image: url('https://images.unsplash.com/photo-1592210454359-9043f067919b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80');
+    background-size: cover;
+    background-position: bottom;
+    transition: 0.4s;
+  }
+
+  main{
+    min-height: 100vh;
+    padding: 25px;
+    background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75) );
+  }
+
+  .search-box{
+    width: 100%;
+    margin-bottom: 30px;
+  }
+
+  .search-box .search-bar {
+    display: block;
+    width: 100%;
+    padding: 15px;
+    color: #313131;
+    font-size: 20px;
+    appearance: none;
+    border: none;
+    outline:none;
+  background-color: rgba(255, 255, 255, 0.15);
   }
 </style>
