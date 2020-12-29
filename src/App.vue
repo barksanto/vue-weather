5a96893ef30eee0be6ec67d4f2158062b40e758c
Barkley Santo <barkley.santo@gmail.com>
2020-12-29 17:39:29 -0600

    using weather object to fill in fields of screen

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,26 @@
+
 <template>
-  <div id="app">
+  <div id="app" :class="typeof weather.main != 'undefined' && weather.main.temp > 16 ? 'warm' : ''">
     <main>
       <div class="search-box">
         <input 
-        type="text" 
-        class="search-bar" 
-        placeholder="search..."
-        v-model="query"
-        @keypress="fetchWeather"
+          type="text" 
+          class="search-bar" 
+          placeholder="Search..."
+          v-model="query"
+          @keypress="fetchWeather"
         />
       </div>
 
-      <div class="weather-wrap">
+      <div class="weather-wrap" v-if="typeof weather.main != 'undefined'">
         <div class="location-box">
-          <div class="location">Boston,Ma</div>
-          <div class="date">Tuesday 29 December</div>
-          <div class="weather-box">
-            <div class="temp">29°F</div>
-            <div class="weather">Rain</div>
-          </div>
+          <div class="location">{{ weather.name }}, {{ weather.sys.country }}</div>
+          <div class="date">{{ dateBuilder() }}</div>
+        </div>
+
+        <div class="weather-box">
+          <div class="temp">{{ Math.round(weather.main.temp) }}°c</div>
+          <div class="weather">{{ weather.weather[0].main }}</div>
         </div>
       </div>
     </main>
@@ -26,117 +28,114 @@
 </template>
 
 <script>
-
-
 export default {
-  name: 'App',
-  data(){
-    return{
-      api_key: 'b7f7ba740725cadece7fa95b9c604600',
-      url_base: 'api.openweathermap.org/data/2.5',
+  name: 'app',
+  data () {
+    return {
+      api_key: '364545f834bbb6e7f867173e9f886040',
+      url_base: 'https://api.openweathermap.org/data/2.5/',
       query: '',
       weather: {}
     }
   },
   methods: {
-    fetchWeather(e) {
-      if (e.key == "Enter"){
-        fetch(`${this.api_base}weather?q=${this.query}`)
+    fetchWeather (e) {
+      if (e.key == "Enter") {
+        fetch(`${this.url_base}weather?q=${this.query}&units=metric&APPID=${this.api_key}`)
+          .then(res => {
+            return res.json();
+          }).then(this.setResults);
       }
+    },
+    setResults (results) {
+      this.weather = results;
     }
   }
 }
-
 </script>
 
 <style>
-  *{
-    margin: 0;
-    padding: 0;
-    box-sizing: border-box;
-  }
-
-  body {
-    font-family: 'montserrat', sans-serif;
-  }
-
-  #app{
-    background-image: url('https://images.unsplash.com/photo-1592210454359-9043f067919b?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80');
-    background-size: cover;
-    background-position: bottom;
-    transition: 0.4s;
-  }
-
-  main{
-    min-height: 100vh;
-    padding: 25px;
-    background-image: linear-gradient(to bottom, rgba(0,0,0,0.25), rgba(0,0,0,0.75) );
-  }
-
-  .search-box{
-    width: 100%;
-    margin-bottom: 30px;
-  }
-
-  .search-box .search-bar {
-    display: block;
-    width: 100%;
-    padding: 15px;
-    color: #313131;
-    font-size: 20px;
-    appearance: none;
-    border: none;
-    outline:none;
-    box-shadow: 0px 0px 16px rgba(0,0,0,0.25);
-    background-color: rgba(255, 255, 255, 0.5);
-    border-radius: 0px 16px 0px 16px;
-    transition :0.4s;
-  }
-
-  .search-box .search-bar:focus{
-    background-color:  rgba(255, 255, 255, 0.75);
-    box-shadow: 0px 0px 16px rgba(0,0,0,0.75);
-    border-radius: 16px 0px 16px 0px;
-  }
-
-  .location-box .location{
-    color: white;
-    font-size: 32px;
-    font-weight: 500;
-    text-align: center;
-    text-shadow: 1px 3px rgba(255, 255, 255, 0.25);
-  }
-
-  .location-box .date {
-    color: white;
-    font-size: 20px;
-    font-weight: 300;
-    text-align: center;
-    font-style: italic;
-  }
-
-  .weather-box{
-    text-align: center;
-  }
-
-  .weather-box .temp {
-    display: inline-block;  /* inline block because we want to add padding and text center it*/
-    padding: 10px 25px;
-    color: white;
-    font-size: 102px;
-    font-weight: 900;
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-    background-color: rgba(255, 255, 255, 0.25);
-    border-radius: 16px;
-    margin: 30px 0;
-    box-shadow: 3px 6px rgba(0, 0, 0, 0.25)
-  }
-
-  .weather-box .weather {
-    color: #fff;
-    font-size: 48px;
-    font-weight: bold;
-    font-style: italic;
-    text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
-  }
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  font-family: 'montserrat', sans-serif;
+}
+#app {
+  background-image: url('https://images.unsplash.com/photo-1575518929362-920773c8e9dc?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=701&q=80');
+  background-size: cover;
+  background-position: bottom;
+  transition: 0.4s;
+}
+#app.warm {
+  background-image: url('https://images.unsplash.com/photo-1505554898845-050c97a90ad6?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1050&q=80');
+}
+main {
+  min-height: 100vh;
+  padding: 25px;
+  background-image: linear-gradient(to bottom, rgba(0, 0, 0, 0.25), rgba(0, 0, 0, 0.75));
+}
+.search-box {
+  width: 100%;
+  margin-bottom: 30px;
+}
+.search-box .search-bar {
+  display: block;
+  width: 100%;
+  padding: 15px;
+  
+  color: #313131;
+  font-size: 20px;
+  appearance: none;
+  border:none;
+  outline: none;
+  background: none;
+  box-shadow: 0px 0px 8px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.5);
+  border-radius: 0px 16px 0px 16px;
+  transition: 0.4s;
+}
+.search-box .search-bar:focus {
+  box-shadow: 0px 0px 16px rgba(0, 0, 0, 0.25);
+  background-color: rgba(255, 255, 255, 0.75);
+  border-radius: 16px 0px 16px 0px;
+}
+.location-box .location {
+  color: #FFF;
+  font-size: 32px;
+  font-weight: 500;
+  text-align: center;
+  text-shadow: 1px 3px rgba(0, 0, 0, 0.25);
+}
+.location-box .date {
+  color: #FFF;
+  font-size: 20px;
+  font-weight: 300;
+  font-style: italic;
+  text-align: center;
+}
+.weather-box {
+  text-align: center;
+}
+.weather-box .temp {
+  display: inline-block;
+  padding: 10px 25px;
+  color: #FFF;
+  font-size: 102px;
+  font-weight: 900;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+  background-color:rgba(255, 255, 255, 0.25);
+  border-radius: 16px;
+  margin: 30px 0px;
+  box-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
+.weather-box .weather {
+  color: #FFF;
+  font-size: 48px;
+  font-weight: 700;
+  font-style: italic;
+  text-shadow: 3px 6px rgba(0, 0, 0, 0.25);
+}
 </style>
